d0ea9268be88628bd14eda6e2b8ffc08eec93ea0
e3-1230v2 <e3-1230v2@linuxcrypt.cn>
2020-04-27 21:56:35 +0800

    feat(kconfig,gpio): build kconfig, add gpio init output dirve led
    rebuild kconfig, move global to submenu. add gpio test
    
    Signed-off-by: e3-1230v2 <e3-1230v2@linuxcrypt.cn>

--- main/fish_tank.c
+++ main/fish_tank.c
@@ -17,6 +17,8 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
+#include "driver/gpio.h"
+
 // smart config include
 #include "tcpip_adapter.h"
 #include "esp_wifi.h"
@@ -32,8 +34,21 @@
 #include "common_nvs.h"
 
 
+#define SMART_CONFIG_BTN    CONFIG_SMART_CONFIG_BTN_PIN
+#define SMART_CONFIG_PIN_SEL  1ULL<<SMART_CONFIG_BTN
+
 static const char *FISH_TANK_TAG = "fish_tank";
 
+static void gpio_smart_config_init()
+{
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = SMART_CONFIG_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+}
 void after_nvs_init_event()
 {
     ESP_LOGD(FISH_TANK_TAG, "after nvs init event");
@@ -46,8 +61,17 @@ void after_nvs_init_event()
  */
 void app_main(void)
 {
+    ESP_LOGD(FISH_TANK_TAG, "nvs init...");
     iot_nvs_init(after_nvs_init_event);
-    ESP_LOGD(FISH_TANK_TAG, "Hello word");
-
     // Check post startup events
+
+    ESP_LOGD(FISH_TANK_TAG, "gpio btn init... %d\n", SMART_CONFIG_BTN);
+    gpio_smart_config_init();
+
+    int cnt = 0;
+    while (1) {
+        ESP_LOGD(FISH_TANK_TAG, "cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(SMART_CONFIG_BTN, cnt % 2);
+    }
 }
