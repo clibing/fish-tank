11ef9d35cad12ab72ec16543ebb9729e4d9141dd
liuboxun <liuboxun@gozap.com>
2020-04-30 14:54:28 +0800

    :bug: fix(1.0): debug mqtt, but ca not pass
    Signed-off-by: liuboxun <liuboxun@gozap.com>

--- main/fish_tank.c
+++ main/fish_tank.c
@@ -32,6 +32,8 @@
 #include "lwip/netdb.h"
 #include "lwip/apps/sntp.h"
 
+#include "mqtt_client.h"
+
 #include "common_nvs.h"
 #include "common_network.h"
 #include "common_oled.h"
@@ -52,6 +54,16 @@ void after_network_connect(int type, int status, char *ip);
 static const char *FISH_TANK_TAG = "fish_tank";
 
 static xQueueHandle gpio_evt_queue = NULL;
+
+static int mqtt_running = 0;
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(FISH_TANK_TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_init();
+}
 
 static void gpio_isr_handler(void *arg) {
     uint32_t gpio_num = (uint32_t) arg;
@@ -116,6 +128,67 @@ void gpio_smart_config_init() {
     ESP_LOGD(FISH_TANK_TAG, "GPIO init OK, detail: SMART_CONFIG_BTN: %d, WATER_PUMP_PIN: %d, O2_PUMP_PIN: %d, LED_PIN: %d ", SMART_CONFIG_BTN, WATER_PUMP_PIN, O2_PUMP_PIN, LED_PIN);
 }
 
+extern const uint8_t fs_ca_pem_start[]   asm("_binary_root_ca_pem_start");
+extern const uint8_t fs_ca_end[]   asm("_binary_root_ca_pem_end");
+
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
+{
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    // your_context_t *context = event->context;
+    switch (event->event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_CONNECTED");
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+            ESP_LOGI(FISH_TANK_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            ESP_LOGI(FISH_TANK_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            ESP_LOGI(FISH_TANK_TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
+
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+            ESP_LOGI(FISH_TANK_TAG, "sent publish successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(FISH_TANK_TAG, "MQTT_EVENT_ERROR");
+            break;
+    }
+    return ESP_OK;
+}
+
+static void mqtt_app_start(void)
+{
+    const esp_mqtt_client_config_t mqtt_cfg = {
+            .uri = CONFIG_BROKER_URI,
+            .event_handle = mqtt_event_handler,
+            .cert_pem = (const char *)fs_ca_pem_start,
+    };
+
+    ESP_LOGI(FISH_TANK_TAG, "root ca value: %s", fs_ca_pem_start);
+    ESP_LOGI(FISH_TANK_TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
 void after_nvs_init_event() {
     ESP_LOGD(FISH_TANK_TAG, "after nvs init event");
 }
@@ -131,6 +204,11 @@ void after_network_connect(int type, int status, char *ip) {
             oled_show_str(1, 1, ":", &Font_7x10, 0);
             oled_show_str(7, 1, ip, &Font_7x10, 0);
         }
+
+        if(mqtt_running == 0){
+            mqtt_app_start();
+            mqtt_running = 1;
+        }
     }
     // mqtt connection...
 }
@@ -143,15 +221,22 @@ void after_network_connect(int type, int status, char *ip) {
 void app_main(void) {
     ESP_LOGD(FISH_TANK_TAG, "nvs init...");
     common_nvs_init(after_nvs_init_event);
+
     // Check post startup events
     oled_init();
     oled_all_on();
     oled_show_str(1, 30, "Loading...", &Font_7x10, 0);
+
     gpio_smart_config_init();
     ESP_LOGD(FISH_TANK_TAG, "gpio btn init... %d\n", SMART_CONFIG_BTN);
+
     // check pin btn smartconfig network press
     oled_show_str(1, 30, "Networking...", &Font_7x10, 0);
-    initialise_wifi(after_network_connect );
+    initialise_wifi(after_network_connect);
+
+    ESP_LOGD(FISH_TANK_TAG, "sntp init...");
+    initialize_sntp();
+
 
 //    int cnt = 0;
 //    while (1) {
